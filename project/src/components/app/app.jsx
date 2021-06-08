f4d330e5812b9d0f2e1d6234d8d1a2948c950014
Kindro5654 <aleks.bochar@gmail.com>
2021-06-08 15:07:05 +0300

    Working with props

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import StartScreen from '../start-screen/start-scren';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  return (
+    <StartScreen name={props.name} genre={props.genre} year={props.year} />
+  );
 }
-
+App.propTypes = {
+  name: PropTypes.string,
+  genre: PropTypes.string,
+  year: PropTypes.string,
+};
 export default App;
