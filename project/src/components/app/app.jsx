d802cb494cfcb8d0316130ca7cc4f3bd36081c6c
Kindro5654 <aleks.bochar@gmail.com>
2021-06-22 01:55:18 +0300

    Building a routing system

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import StartScreen from '../start-screen/start-scren';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import MoviePage from '../movie-page/movie-page';
+import MyList from '../my-list/my-list';
+import NotFound from '../not-found/not-found';
+import Player from '../player/player';
+import SignIn from '../sign-In/sign-in';
 
 function App(props) {
   return (
-    <StartScreen name={props.name} genre={props.genre} year={props.year} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.MAIN}>
+          <StartScreen name={props.name} genre={props.genre} year={props.year} />
+        </Route>
+        <Route exact path={AppRoute.FILM}>
+          <MoviePage />
+        </Route>
+        <Route exact path={AppRoute.MY_LIST}>
+          <MyList />
+        </Route>
+        <Route exact path={AppRoute.PLAYER}>
+          <Player />
+        </Route>
+        <Route exact path={AppRoute.SIGN_IN}>
+          <SignIn />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 App.propTypes = {
